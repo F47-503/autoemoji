0fbaa6c1e91124872023da4ae30814b0b53a3925
Vitaly Kurin <vitaly14kurin@gmail.com>
2023-10-04 07:26:11 +0300

    data mining reworked, flag for channels added, label creation moved to to_csv

--- data.py
+++ data.py
@@ -4,25 +4,35 @@ import uvloop
 import tgcrypto
 import pandas as pd
 import pickle
-api_data = open('tg_credentials').read().split('\n')
+
+api_data = open("tg_credentials").read().split("\n")
 api_id = int(api_data[0])
 api_hash = api_data[1]
 uvloop.install()
 app = pyrogram.Client(api_id=api_id, api_hash=api_hash, name="data")
 messages_reactions = []
+
+
 def main():
-  cnt = 0
-  for chat in app.get_dialogs():
-    if chat.chat.title:
-      for mess in app.get_chat_history(chat_id=chat.chat.id):
-        if mess.text and mess.reactions:
-          cnt += 1
-          if cnt % 3000 == 0:
-            print(cnt)
-          messages_reactions.append([mess.text, mess.reactions.reactions])
-      print('next chat')
+    cnt = 0
+    for chat in app.get_dialogs():
+        if chat.chat.title:
+            for mess in app.get_chat_history(chat_id=chat.chat.id):
+                if mess.text and mess.reactions:
+                    cnt += 1
+                    if cnt % 3000 == 0:
+                        print(cnt)
+                    messages_reactions.append(
+                        [
+                            mess.text,
+                            mess.reactions.reactions,
+                            chat.chat.type is pyrogram.enums.ChatType.GROUP,
+                        ]
+                    )
+            print("next chat")
+
+
 app.start()
 main()
-f = open('data_raw', 'wb')
-pickle.dump(messages_reactions, f)
-f.close()
+with open("data_raw", "wb") as f:
+    pickle.dump(messages_reactions, f)
--- to_csv.py
+++ to_csv.py
@@ -1,19 +1,39 @@
 import pandas as pd
 import numpy as np
 import pickle
-data = pickle.load(open('data_raw', 'rb'))
+
+data = pickle.load(open("data_raw", "rb"))
 emoji_stats = {}
 for entry in data:
-  emoji_list = entry[1]
-  for emoji in emoji_list:
-    if not emoji.emoji in emoji_stats:
-      emoji_stats[emoji.emoji] = 0
-    emoji_stats[emoji.emoji] += emoji.count
-df = pd.DataFrame([[entry[0], sum([x.count for x in entry[1]])] for entry in data], columns=['text', 'total'])
+    emoji_list = entry[1]
+    for emoji in emoji_list:
+        if not emoji.emoji in emoji_stats:
+            emoji_stats[emoji.emoji] = 0
+        emoji_stats[emoji.emoji] += emoji.count
+df = pd.DataFrame(
+    [[entry[0], sum([x.count for x in entry[1]]), entry[2]] for entry in data],
+    columns=["text", "total", "chat_type"],
+)
 for emoji in emoji_stats:
-  df[emoji] = pd.Series(np.zeros(df.shape[0]))
+    df[emoji] = pd.Series(np.zeros(df.shape[0]))
 for entry in df.index:
-  emoji_list = data[entry][1]
-  for emoji in emoji_list:
-    df[emoji.emoji][entry] += emoji.count / df['total'][entry]
-df.to_csv('data_all.csv', index=False)
+    emoji_list = data[entry][1]
+    for emoji in emoji_list:
+        df[emoji.emoji][entry] += emoji.count / df["total"][entry]
+df["label"] = pd.Series(
+    map(
+        lambda x: df.columns[3:][x],
+        np.argmax(df.loc[:, df.columns[3:]].to_numpy(), axis=1),
+    )
+)
+df = df[~df['label'].isna()]
+emoji_counts = df["label"].value_counts().head()
+poor_presented_emojis = pd.Series(
+    [emoji for emoji in emoji_counts.index if emoji_counts[emoji] < 200]
+)
+df = df[~df["label"].isin(poor_presented_emojis)]
+df.drop(poor_presented_emojis, axis=1, inplace=True)
+emoji_counts = emoji_counts[~emoji_counts.index.isin(poor_presented_emojis)]
+df.to_csv("data_all.csv", index=False)
+with open('emoji_list', 'wb') as emoji_file:
+    pickle.dump(emoji_counts.index, emoji_file)
